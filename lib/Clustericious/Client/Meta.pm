4621be2423e5c430cf1859b34f90c71fd4e80ddf
Brian Duggan <brian.duggan@nasa.gov>
2010-12-15 12:49:57 -0500

    added api to common commands

--- lib/Clustericious/Client/Meta.pm
+++ lib/Clustericious/Client/Meta.pm
@@ -14,7 +14,7 @@ use warnings;
 
 our %Routes; # hash from class name to array ref of routes.
 our %Objects; # hash from class name to array ref of objects.
-our @CommonRoutes = ( [ "version" => '' ], [ "status" => '' ] );
+our @CommonRoutes = ( [ "version" => '' ], [ "status" => '' ], [ "api" => '' ] );
 
 =item add_route
 
